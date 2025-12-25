66beef874c40af902d8af8bfd78a566f33dd2654
lillia05 <indahlialekok@gmail.com>
2025-12-25 14:36:32 +0700

    buat file login

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// 1. Redirect halaman awal langsung ke Login
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
+});
+
+// 2. Rute untuk menampilkan Halaman Login (View yang kamu buat tadi)
+Route::get('/login', function () {
+    // Pastikan file view kamu ada di folder: resources/views/auth/login.blade.php
+    return view('auth.login'); 
+})->name('login');
+
+// 3. Rute simulasi post login (Biar kalau tombol diklik tidak error 404)
+Route::post('/login', function () {
+    return "Ini simulasi submit login. Nanti Backend yang urus fungsi aslinya.";
 });
