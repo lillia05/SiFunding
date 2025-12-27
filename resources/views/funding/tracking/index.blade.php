8c2346ebf02b23cffbe5ce443c2a9598af7b33ab
lillia05 <indahlialekok@gmail.com>
2025-12-27 11:40:28 +0700

    buat halaman detail di tracking

--- resources/views/funding/tracking/index.blade.php
+++ resources/views/funding/tracking/index.blade.php
@@ -124,7 +124,10 @@
                             20 Des, 10:00
                         </td>
                         <td class="px-6 py-4 whitespace-nowrap text-center text-sm font-medium">
-                            <a href="#" class="text-gray-400 hover:text-gray-600">Detail</a>
+                            <a href="{{ route('tracking.show') }}" class="inline-flex items-center px-3 py-1 bg-white border border-gray-300 rounded-full text-xs font-medium text-gray-700 hover:bg-gray-50 hover:text-bsi-teal transition shadow-sm">
+                                <svg class="w-3 h-3 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"></path></svg>
+                                Detail
+                            </a>
                         </td>
                     </tr>
 
