ab8139db2233c814229c9cbb6529ffddd625b343
febrina1602 <112796775+febrina1602@users.noreply.github.com>
2026-01-27 21:18:37 +0700

    Edit RoleMiddleware

--- app/Http/Middleware/RoleMiddleware.php
+++ app/Http/Middleware/RoleMiddleware.php
@@ -8,18 +8,11 @@ use Symfony\Component\HttpFoundation\Response;
 
 class RoleMiddleware
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     */
     public function handle(Request $request, Closure $next, ...$roles): Response
     {
-        // Cek user sudah login dan memiliki role yang diizinkan
         if (!$request->user() || !in_array($request->user()->role, $roles)) {
             abort(403, 'Unit kerja Anda tidak memiliki izin untuk akses halaman ini.');
         }
-
         return $next($request);
     }
 }
