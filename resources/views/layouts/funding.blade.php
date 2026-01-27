017db25e1742865a7c65718d56a23aa9cb251544
lillia05 <indahlialekok@gmail.com>
2026-01-27 07:55:28 +0700

    benerin sidebar tampilan mobile

--- resources/views/layouts/funding.blade.php
+++ resources/views/layouts/funding.blade.php
@@ -23,16 +23,24 @@
 
     <script src="//unpkg.com/alpinejs" defer></script>
 </head>
-<body class="bg-gray-50 font-sans antialiased text-gray-800">
+<body class="bg-gray-50 font-sans antialiased text-gray-800" x-data="{ sidebarOpen: false }"> <div class="flex h-screen overflow-hidden">
 
-    <div class="flex h-screen overflow-hidden">
+        <div x-show="sidebarOpen" 
+             @click="sidebarOpen = false" 
+             x-transition:enter="transition-opacity ease-linear duration-300" 
+             x-transition:enter-start="opacity-0" 
+             x-transition:enter-end="opacity-100" 
+             x-transition:leave="transition-opacity ease-linear duration-300" 
+             x-transition:leave-start="opacity-100" 
+             x-transition:leave-end="opacity-0"
+             class="fixed inset-0 bg-gray-900 bg-opacity-50 z-20 md:hidden">
+        </div>
 
         @include('components.sidebar')
 
-        <main class="flex-1 overflow-y-auto bg-gray-50/50">
-            <div class="md:hidden h-16 bg-white border-b flex items-center px-4 justify-between sticky top-0 z-20">
-                <img class="h-6" src="https://upload.wikimedia.org/wikipedia/commons/a/a0/Bank_Syariah_Indonesia.svg" alt="Logo">
-                <button class="text-gray-500">
+        <main class="flex-1 overflow-y-auto bg-gray-50/50 relative"> <div class="md:hidden h-16 bg-white border-b flex items-center px-4 justify-between sticky top-0 z-10"> <img class="h-6" src="https://upload.wikimedia.org/wikipedia/commons/a/a0/Bank_Syariah_Indonesia.svg" alt="Logo">
+                
+                <button @click="sidebarOpen = !sidebarOpen" class="text-gray-500 focus:outline-none">
                     <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
                 </button>
             </div>
